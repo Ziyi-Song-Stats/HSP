d7f3298e28c7b81715f674ac202aaeac49c98693
Ziyi Song <89238866+Ziyi-Song-Stats@users.noreply.github.com>
2024-03-29 21:30:42 -0700

    Add files via upload

---
+++ compare_BCPlaid/HSP/SimData.R
@@ -0,0 +1,18 @@
+#####
+SimData = function(seedsSet){
+  set.seed(seedsSet)
+  J=10
+  I=30
+  labels.matrix = matrix(NA, nrow=I, ncol=J)
+  for (j in 1:J){
+    labels.matrix[ , j] = c(rep(1,10), rep(2,10), rep(3,10))
+  }
+  data = matrix(NA, nrow=I, ncol=J)
+  data[labels.matrix == 1] = rnorm(I*J/3, -0.97, 0.4)
+  data[labels.matrix == 2] = rnorm(I*J/3, 0.15, 0.4)
+  data[labels.matrix == 3] = rnorm(I*J/3, 1.37, 0.4)
+
+  return(list("data" = data,
+              "labels.matrix" = labels.matrix))
+}
+
